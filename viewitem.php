86596e7d00031290e30ee834506f42b746aff903
David McDonald <idavidmcdonald@me.com>
2014-08-20 09:32:17 -0700

    front end improvements

--- viewitem.php
+++ viewitem.php
@@ -32,20 +32,17 @@ require "catalogue.php";
 	$item = $catalogue -> ItemExists($item_id);
 
 // Output item details
-	echo $item -> getName();
-	echo "<br>";
-	echo $item -> getPrice();
-	echo "<br>";
-	printf('<a href = "additem.php?id=%s">Add item to cart</a><br>', $item -> getId());
-	printf('<a href = "cart.php">View my cart (%d items)</a>', $cart -> count());
-
 ?>
 
 <div class="panel panel-default">
   <div class="panel-heading">
-    <h3 class="panel-title"><?= $item -> getName() ?></h3>
+    <h3 class="panel-title"><?= $item -> getName() ?>
+    <span class="panel-title pull-right">$<?= $item -> getPrice() ?></span></h3>
   </div>
   <div class="panel-body">
+  	<p>
+  		<img src = "placeholder.png" class = "img-thumbnail" style = "height: 200px">
+  	</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porttitor sapien id tortor tempus, lacinia malesuada lorem suscipit. Suspendisse tincidunt risus id ultrices viverra. Quisque at bibendum libero, quis aliquet velit. Curabitur eu porta magna, sit amet auctor augue. Aenean porta facilisis laoreet. Nam vel mollis nisi. Pellentesque et consectetur velit, ac pharetra justo. Cras ac massa eu nibh convallis eleifend. Sed volutpat tempor interdum. Vestibulum adipiscing, arcu sed lacinia mattis, sapien est consequat metus, ac suscipit dui elit id mauris. Curabitur sed sodales lectus. Aenean id magna egestas, mattis diam sed, sodales erat.
 
 In hac habitasse platea dictumst. Vivamus eleifend lacinia mauris vel fermentum. Sed ac lacus sed mi tempus vehicula quis in sapien. Morbi a dapibus magna. Sed gravida molestie tempor. Mauris velit nisi, faucibus ac interdum eu, ultrices accumsan risus. Sed metus arcu, scelerisque non nibh eget, porttitor fermentum nulla.
