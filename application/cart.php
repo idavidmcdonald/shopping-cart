d417aef7c71f58ab927be4698a6d389157a0c0ba
David McDonald <idavidmcdonald@me.com>
2014-08-24 23:10:23 -0700

    removed php echo of html

--- application/cart.php
+++ application/cart.php
@@ -5,8 +5,8 @@
 
 // Output cart contents
 	if (!$cart -> isEmpty()) {
-		// Cart heading
 			?>
+				<!-- Cart Heading -->
 				<div class = "col-md-12">
 					<h2>Cart Contents (<?= count($cart) ?> items)</h2>
 
@@ -19,38 +19,48 @@
 						</tr>
 
 			<?
-		// Cart contents
 			foreach ($cart as $arr) {
 		        $item = $arr['item'];
-		        
-		        // Output item row
-			        echo "<tr>";
-			        // Item name (as a link)
-			        	printf('<td><a href = "viewitem.php?id=%d">%s</a></td>', $item->getId(), $item->getName());
-			        // Item price
-			        	printf('<td>$%0.2f</td>', $item->getPrice());
-			        // Item quantity
-						printf('<td>%d</td>', $arr['qty']);
-					// Add one item button
-			        	printf('<td><a href = "additem.php?id=%s"><button type="button" class="btn btn-success"><b>+</b></button></a>', $item->getId());
-			        // Remove one item button
-			        	printf(' <a href = "removeitem.php?id=%s"><button type="button" class="btn btn-danger"><b>-</b></button></a></td>', $item->getId());
-			        echo "</tr>";
-		    }
+		    ?>
+		    		<!-- Item row -->
+					<tr>
+						<!-- Item name -->
+			        	<td><? printf('<a href = "viewitem.php?id=%d">%s</a>', $item->getId(), $item->getName()) ?></td>
 
-		// Cart total
-		    echo "<tr>";
-		    printf('<td><strong>Total: $%0.2f<strong></td>', $cart -> getTotal());
-		    echo "<td></td><td></td><td></td></tr>";
-		    echo "</table>";
-		    echo '<button type="button" class="btn btn-primary">Checkout</button>';	
-		    echo "</div>";	    
-	} else {
-		// Empty cart heading
-			echo "<div class = 'col-md-12'>";
-			echo "<h2>Cart is empty</h2>";
-			echo "</div>";
-	}
+			        	<!-- Item price -->
+			        	<td>$<? printf('%0.2f', $item->getPrice()) ?></td>
+
+			        	<!-- Item quantity -->
+						<td><? printf('%d', $arr['qty'])?></td>
+
+			        	<td> 
+			        		<!-- Add one item button -->
+			        		<? printf('<a href = "additem.php?id=%s">', $item->getId()) ?> <button type="button" class="btn btn-success"><b>+</b></button></a>
+			        		<!-- Remove one item button -->
+			        		<? printf('<a href = "removeitem.php?id=%s">', $item->getId()) ?> <button type="button" class="btn btn-danger"><b>-</b></button></a>
+			        	</td>
+			        </tr>
+
+			 <? } ?>
+
+			<!-- Cart Total -->
+		    <tr>
+		    	<td><strong>Total:<? printf(' $%0.2f<strong>', $cart -> getTotal()) ?></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		</table>
+		<button type="button" class="btn btn-primary">Checkout</button>
+	</div>
+
+	<? } else { ?>
+		<!-- Empty cart heading -->
+		<div class = 'col-md-12'>
+			<h2>Cart is empty</h2>
+		</div>
+
+	<? } 
 
 // HTML store footer
 	require 'footer.html';
